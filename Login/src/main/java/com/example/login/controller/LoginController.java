610d431a9234d5267b1aa2fd1a9a2b5532c12acd
João Mário Abreu <abreujoaomr@gmail.com>
2026-09-03 15:57:37 -0300

    Add files via upload

---
+++ Login/src/main/java/com/example/login/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+}
